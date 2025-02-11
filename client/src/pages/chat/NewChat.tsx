c1119e10c49fd10003fd8a93d5ee503640f4c803
mahesh jamdade <maheshmn121@gmail.com>
2025-02-11 09:02:32 -0500

    show sidebar on new chat

--- client/src/pages/chat/NewChat.tsx
+++ client/src/pages/chat/NewChat.tsx
@@ -3,12 +3,18 @@ import ChatInput from "../../components/ChatInput";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { addUserMessage, sendMessage, setNewChat, streamResponse } from "../../redux/reducers/chatSlice";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 import { AppDispatch, RootState } from "src/redux/store";
 import { createChat } from "src/redux/reducers/userChatsSlice";
 import { useSelector } from "react-redux";
-import { setChatId } from "src/redux/reducers/appSlice";
+import { setAgent, setChatId } from "src/redux/reducers/appSlice";
 import { LLM_Provider } from "src/services/ResponseProvider";
+import { Navbar } from "@nextui-org/react";
+import IconButton from "src/components/IconButton";
+import Menu from "src/components/Menu";
+import { FiSidebar } from "react-icons/fi";
+import Constants, { Agent } from "src/utils/Constants";
+import { toggleSidebar } from "src/redux/reducers/sidebarSlice";
 interface NewChatProps { }
 
 const NewChat: React.FC<NewChatProps> = () => {
@@ -17,7 +23,8 @@ const NewChat: React.FC<NewChatProps> = () => {
     const app = useSelector((state: RootState) => state.app);
     const auth = useSelector((state: RootState) => state.auth)
     const agent = useSelector((state: RootState) => state.app.agent.valueOf());
-    
+    const sidebar = useSelector((state: RootState) => state.sidebar);
+    const location = useLocation(); //
     const handleCreateChat = async (query: string) => {
         if (auth.user) {
             console.log("Creating new chat...");
@@ -76,30 +83,59 @@ const NewChat: React.FC<NewChatProps> = () => {
         }
     };
 
+    const handleMenuChange = (value: string) => {
+        const selectedAgent: Agent = Constants.items[value];
+        dispatch(setAgent(selectedAgent));
+    };
+
+
     return (
-        <motion.div
-            className="flex justify-center items-center h-screen overflow-hidden"
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 50, opacity: 1 }}
-            exit={{ y: 100, opacity: 0 }}
-            transition={{
-                type: "spring",
-                damping: 20,
-                stiffness: 100,
-                duration: 0.8,
-            }}
-        >
-            <div className="max-w-3xl w-full">
-                <div className="flex justify-center">
-                    <p className="text-xl my-4">AI Chatbot</p>
-                </div>
-                <ChatInput
-                    className="w-full z-20 mb-8"
-                    onSend={handleSend}
-                    onNewChatCreate={handleCreateChat}
-                />
+        <div className="flex flex-col">
+            <div className="flex-none sticky top-0 z-10 w-full">
+                <Navbar className="w-full">
+                    <div className="flex items-center justify-between w-full">
+                        {!sidebar.isOpen && location.pathname === '/' ? (
+                            <IconButton
+                                ariaLabel="Sidebar"
+                                onClick={() => dispatch(toggleSidebar())}
+                            >
+                                <FiSidebar className="w-4 h-4 text-black" />
+                            </IconButton>
+                        ) : (
+                            <div />
+                        )}
+                        <Menu
+                            onChange={handleMenuChange}
+                            options={Object.keys(Constants.items).map((key) => ({ value: key, label: key }))}
+                            placeholder="Next.js"
+                        />
+                    </div>
+                </Navbar>
             </div>
-        </motion.div>
+            <motion.div
+                className="flex justify-center items-center h-screen overflow-hidden"
+                initial={{ y: 100, opacity: 0 }}
+                animate={{ y: 50, opacity: 1 }}
+                exit={{ y: 100, opacity: 0 }}
+                transition={{
+                    type: "spring",
+                    damping: 20,
+                    stiffness: 100,
+                    duration: 0.8,
+                }}
+            >
+                <div className="max-w-3xl w-full">
+                    <div className="flex justify-center">
+                        <p className="text-xl my-4">AI Chatbot</p>
+                    </div>
+                    <ChatInput
+                        className="w-full z-20 mb-8"
+                        onSend={handleSend}
+                        onNewChatCreate={handleCreateChat}
+                    />
+                </div>
+            </motion.div>
+        </div>
     );
 };
 
